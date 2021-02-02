f4f17ec0f822326e7fe7ae16de10db2f7bc4b971
Pablo H <pablo.hm9@gmail.com>
2021-02-02 22:30:35 +0100

    Content 2.7, 02.02.2021

---
+++ app/src/main/java/com/example/todoapp/fragments/list/ListFragment.kt
@@ -0,0 +1,27 @@
+package com.example.todoapp.fragments.list
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.todoapp.R
+
+class ListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_list, container, false)
+
+        view.floatingActionButton.setOnClickListener {
+            findNavController().navigate(R.id.action_listFragment_to_addFragment)
+        }
+
+        return view
+    }
+}
