e59f85600232cd6a2735d2fc7b4af95aae97d1cc
Pablo H <pablo.hm9@gmail.com>
2021-02-04 20:12:23 +0100

    3.19 Create Converter (Converter), 04.02.2021

--- app/src/main/java/com/example/todoapp/data/ToDoDatabase.kt
+++ app/src/main/java/com/example/todoapp/data/ToDoDatabase.kt
@@ -1,4 +1,34 @@
 package com.example.todoapp.data
 
-class ToDoDatabase {
+import android.content.Context
+import androidx.room.*
+
+@Database(entities = [ToDoData::class], version = 1, exportSchema = false)
+@TypeConverters(Converter::class)
+abstract class ToDoDatabase : RoomDatabase() {
+
+    abstract fun toDoDao(): ToDoDao
+
+    companion object {
+
+        @Volatile
+        private var INSTANCE: ToDoDatabase? = null
+
+        fun getDatabase(context: Context): ToDoDatabase {
+            val tempInstance = INSTANCE
+            if (tempInstance != null) {
+                return tempInstance
+            }
+
+            synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    ToDoDatabase::class.java,
+                    "todo_base"
+                ).build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
 }
