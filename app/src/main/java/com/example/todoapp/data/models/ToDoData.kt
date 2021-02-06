cbc5773386cde9baf3633cd46c85b354190bc8db
Pablo H <pablo.hm9@gmail.com>
2021-02-06 17:50:48 +0100

    3.29 Safe Args - Pass ToDoData object to UpdateFragment

--- app/src/main/java/com/example/todoapp/data/models/ToDoData.kt
+++ app/src/main/java/com/example/todoapp/data/models/ToDoData.kt
@@ -1,14 +1,16 @@
 package com.example.todoapp.data.models
 
+import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import com.example.todoapp.data.models.Priority
+import kotlinx.android.parcel.Parcelize
 
 @Entity(tableName = "todo_table")
+@Parcelize
 data class ToDoData(
-        @PrimaryKey(autoGenerate = true)
+    @PrimaryKey(autoGenerate = true)
     var id: Int,
-        var title: String,
-        var priority: Priority,
-        var description: String
-)
+    var title: String,
+    var priority: Priority,
+    var description: String
+) : Parcelable
