8a9de0fadc0cd48ea41e4d30cbcff2bc38d3f972
Pablo H <pablohema@pop-os.localdomain>
2021-02-10 20:47:32 +0100

    7.41 Add Animation to RecyclerView

--- app/src/main/java/com/example/todoapp/fragments/list/ListFragment.kt
+++ app/src/main/java/com/example/todoapp/fragments/list/ListFragment.kt
@@ -17,6 +17,8 @@ import com.example.todoapp.databinding.FragmentListBinding
 import com.example.todoapp.fragments.SharedViewModel
 import com.example.todoapp.fragments.list.adapter.ListAdapter
 import com.google.android.material.snackbar.Snackbar
+import jp.wasabeef.recyclerview.animators.LandingAnimator
+import jp.wasabeef.recyclerview.animators.SlideInUpAnimator
 import java.text.FieldPosition
 
 class ListFragment : Fragment() {
@@ -62,6 +64,9 @@ class ListFragment : Fragment() {
         val recyclerView = binding.recyclerView
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(requireActivity())
+        recyclerView.itemAnimator = LandingAnimator().apply {
+            addDuration = 300
+        }
 
         // Swipe to delete
         swipeToDelete(recyclerView)
